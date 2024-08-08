7164adfa7124264b2fdef9014dd6623c91fb2341
Alphahime <alphandiaye383@gmail.com>
2024-08-08 11:44:49 +1000

    Version incluant les commentaires

--- src/app/post/comment.ts
+++ src/app/post/comment.ts
@@ -1,8 +1,7 @@
 // src/app/comment.ts
 export interface Comment {
-    id: number;
-    postId: number;
-    author: string;
-    content: string;
-  }
-  
+  id: number;
+  postId: number;
+  author: string;
+  content: string;
+}
